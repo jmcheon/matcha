7fb5879c97d050126f50187a3694b7d42f10f4f4
Sungyong Cho <dev@sungyongcho.com>
2024-02-07 18:49:13 +0100

    chore: added tailwind config

---
+++ front/pages/index2.vue
@@ -0,0 +1,17 @@
+<script setup>
+// const onUpdate = t => {}
+
+// const { counter, state } = useAdder()
+
+import { storeToRefs } from 'pinia';
+import { counterStore } from '@/stores/counterStore';
+
+const { state } = storeToRefs(counterStore());
+const store = counterStore();
+</script>
+<template>
+  <main>
+    <button @click="counterStore().counter">Click</button>
+    {{ state }}
+  </main>
+</template>
--- front/pages/todo.vue
+++ front/pages/todo.vue
@@ -1,72 +1,57 @@
-<script>
-import { ref, onMounted } from 'vue';
-
-export default {
-  name: 'App',
-  setup() {
-    const newTodo = ref('');
-    const defaultData = [
-      {
-        done: false,
-        content: 'Write a blog post',
-      },
-    ];
-    const todosData = JSON.parse(getLocalStorage('todos')) || defaultData;
-    const todos = ref(todosData);
-
-    function getLocalStorage(key) {
-      if (typeof window !== 'undefined') {
-        return localStorage.getItem(key);
-      }
-      return null;
-    }
-
-    function setLocalStorage(key, value) {
-      if (typeof window !== 'undefined') {
-        localStorage.setItem(key, value);
-      }
-    }
-
-    function addTodo() {
-      if (newTodo.value) {
-        todos.value.push({
-          done: false,
-          content: newTodo.value,
-        });
-        newTodo.value = '';
-      }
-      saveData();
-    }
-
-    function doneTodo(todo) {
-      todo.done = !todo.done;
-      saveData();
-    }
-
-    function removeTodo(index) {
-      todos.value.splice(index, 1);
-      saveData();
-    }
-
-    function saveData() {
-      const storageData = JSON.stringify(todos.value);
-      setLocalStorage('todos', storageData);
-    }
-
-    // Trigger saveData on component mount to initialize localStorage
-    onMounted(() => {
-      saveData();
-    });
-
-    return {
-      todos,
-      newTodo,
-      addTodo,
-      doneTodo,
-      removeTodo,
-    };
+<script setup>
+const newTodo = ref('');
+const defaultData = [
+  {
+    done: false,
+    content: 'Write a blog post',
   },
-};
+];
+const todosData = JSON.parse(getLocalStorage('todos')) || defaultData;
+const todos = ref(todosData);
+
+function getLocalStorage(key) {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(key);
+  }
+  return null;
+}
+
+function setLocalStorage(key, value) {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(key, value);
+  }
+}
+
+function addTodo() {
+  if (newTodo.value) {
+    todos.value.push({
+      done: false,
+      content: newTodo.value,
+    });
+    newTodo.value = '';
+  }
+  saveData();
+}
+
+function doneTodo(todo) {
+  todo.done = !todo.done;
+  saveData();
+}
+
+function removeTodo(index) {
+  todos.value.splice(index, 1);
+  saveData();
+}
+
+function saveData() {
+  const storageData = JSON.stringify(todos.value);
+  setLocalStorage('todos', storageData);
+}
+
+// Trigger saveData on component mount to initialize localStorage
+onMounted(() => {
+  saveData();
+});
 </script>
 
 <template>
@@ -75,9 +60,9 @@ export default {
     <form @submit.prevent="addTodo()">
       <label>New ToDo </label>
       <input v-model="newTodo" name="newTodo" autocomplete="off" />
-      <button>Add ToDo</button>
+      <button class="text">Add ToDo</button>
     </form>
-    <h2>ToDo List</h2>
+    <h2 :class="$style.text2">ToDo List</h2>
     <ul>
       <li v-for="(todo, index) in todos" :key="index">
         <span :class="{ done: todo.done }" @click="doneTodo(todo)">{{ todo.content }}</span>
@@ -87,3 +72,15 @@ export default {
     <h4 v-if="todos.length === 0">Empty list.</h4>
   </div>
 </template>
+
+<style scoped>
+.text {
+  color: blue;
+}
+</style>
+
+<style module>
+.text2 {
+  color: red;
+}
+</style>

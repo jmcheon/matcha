b2d04b92862dadd013c36852bc3cfa1d89d7586e
Sungyong Cho <dev@sungyongcho.com>
2024-02-07 22:34:31 +0100

    chore: fix index.vue not working correctly

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,18 +1,18 @@
-<script>
-import 'primevue/resources/themes/aura-light-green/theme.css';
-
+<script setup>
 // const onUpdate = t => {}
+
 // const { counter, state } = useAdder()
+
 import { storeToRefs } from 'pinia';
 
 import { counterStore } from '@/stores/counterStore';
-const { state } = storeToRefs(counterStore());
 
-// const store = counterStore()
+const { state } = storeToRefs(counterStore());
+const store = counterStore();
 </script>
 <template>
   <main>
-    <Button @click="counterStore().counter"> Click </Button>
+    <button @click="counterStore().counter">Click</button>
     {{ state }}
   </main>
 </template>

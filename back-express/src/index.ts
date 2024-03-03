d21f6058f94bfd08485427399e38f15331aaef19
jungmoo cheon <developer.cheon@gmail.com>
2024-03-03 06:52:12 +0100

    feat: add expose port of back and front for dev environment and mysql test done

--- back-express/src/index.ts
+++ back-express/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");

6164669563021d9c3c68868d32224544d8620ad3
jungmoo cheon <developer.cheon@gmail.com>
2024-03-05 13:22:17 +0100

    feat: login selector vue

--- front-nuxt/components/LoginSelector.vue
+++ front-nuxt/components/LoginSelector.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { onClickOutside } from '@vueuse/core';
   const { t } = useI18n();
   const options = computed(() => [
     {
@@ -14,18 +15,26 @@
   const toggleDropdown = () => {
     showDropdown.value = !showDropdown.value;
   };
+  const target = ref(null);
+  onClickOutside(target, () => {
+    showDropdown.value = false;
+  });
 </script>
 
 <template>
-  <div>
-    <button @click.stop="toggleDropdown">login</button>
-    <ul v-if="showDropdown">
-      <li
-        v-for="option in options"
-        :key="option.value"
-        class="absolute top-[calc(100%+10px)] right-0 gap-1 bg-white rounded-lg p-1 flex flex-col"
-      >
-        <div>
+  <div ref="target" class="relative">
+    <v-btn
+      icon="mdi-account"
+      aria-label="User Login"
+      @click.stop="toggleDropdown"
+    />
+    <ul
+      v-if="showDropdown"
+      :class="$style.dropdown"
+      class="absolute min-w-[80px] top-[calc(100%+10px)] right-0 gap-1 rounded-lg bg-white text-balck p-1 flex flex-col"
+    >
+      <li v-for="option in options" :key="option.value">
+        <div class="flex w-full rounded-lg px-2 py-1 hover:bg-surface-200">
           {{ option.name }}
         </div>
       </li>
@@ -33,11 +42,17 @@
   </div>
 </template>
 
-<!-- <sytle module lang="scss">
-.dropdown::before {
-  position: absolute;
-  right: 15px;
-  bottom: 100%;
-  display: block;
-}
-</sytle> -->
+<style module>
+  .dropdown::before {
+    position: absolute;
+    right: 15px;
+    bottom: 100%;
+    display: block;
+    width: 0;
+    height: 0;
+    border-right: 10px solid transparent;
+    border-bottom: 10px solid white;
+    border-top: 10px solid transparent;
+    content: '';
+  }
+</style>
--- front-nuxt/components/TheNavbar.vue
+++ front-nuxt/components/TheNavbar.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <nav class="top-0 z-[999] h-[112px] w-full">
+  <nav class="top-0 z-[999] h-[112px] w-full bg-black">
     <div class="mx-auto flex max-w-[1280px] justify-between p-4">
       <div>Lang</div>
       <div class="absolute left-[50%]">Logo</div>

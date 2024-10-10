0bc896f415b76a3283afa40f216b7c195e16b072
Sungyong Cho <dev@sungyongcho.com>
2024-10-10 17:26:10 +0200

    wip: generate profile done; need to refactor

--- back-fastapi/src/controllers/profile_controller.py
+++ back-fastapi/src/controllers/profile_controller.py
@@ -4,10 +4,12 @@ import src.repositories.profile_repository as profile_repository
 import src.services.auth_service as auth_service
 from fastapi import APIRouter, Cookie, HTTPException, Response, status
 from fastapi.responses import JSONResponse
+from src.models.dtos.profile_dto import GenerateProfileDTO
 
 router = APIRouter(tags=["Profile"])
 
 
+# TODO: token validation
 @router.get("/profile", status_code=status.HTTP_200_OK, response_model=None)
 async def get_profile(res: Response, accessToken: str = Cookie(None)) -> Optional[dict]:
     if accessToken is None:
@@ -22,8 +24,11 @@ async def get_profile(res: Response, accessToken: str = Cookie(None)) -> Optiona
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
 
 
+# TODO: token validation / profile field validation / account existence verification
 @router.post("/profile", status_code=status.HTTP_200_OK, response_model=None)
-async def generate_profile(res: Response, accessToken: str = Cookie(None)) -> Optional[dict]:
+async def generate_profile(
+    res: Response, data: GenerateProfileDTO, accessToken: str = Cookie(None)
+) -> Optional[dict]:
     if accessToken is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -32,8 +37,12 @@ async def generate_profile(res: Response, accessToken: str = Cookie(None)) -> Op
     try:
         payload = auth_service.decode_token(accessToken)
         print("payload", payload)
+        await profile_repository.insert_profile(payload["accountId"], data)
+        profile = await profile_repository.get_profile(payload["accountId"])
+        print("geneated profile", profile)
         return None
-    except Exception:
+    except Exception as e:
+        print("what is e", e)
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"code": "GENERAL_ERROR"}
         )
